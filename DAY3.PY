d1d5cc0b8796f5aac62a9784ecde344c0e7c8a6c
GoddatiSaiRamCharan <83704592+GoddatiSaiRamCharan@users.noreply.github.com>
2021-05-08 19:29:22 +0530

    Add files via upload

---
+++ DAY3.PY
@@ -0,0 +1,14 @@
+a = int(input("Enter operand one: "))
+b = int(input("Enter operand two: "))
+print(f"a+b: {a+b}")
+print(f"a-b: {a-b}")
+print(f"a*b: {a*b}")
+if b == 0:
+    print("Division by 0 is not possible!")
+else:
+    print(f"a/b: {a/b}")
+print(f"a**b: {a**b}")
+
+
+Dob = int(input("Enter your Date of birth: "))
+print(f"your age is: {2021-Dob}")
---
+++ DAY4.PY
@@ -0,0 +1,5 @@
+user_input = input("Enter a word to count'y' in it: ").lower()
+print(user_input.count('y'))
+swap_case = input("Enter a string to swap case: ")
+print(swap_case[::2])
+print(swap_case.swapcase())
